10c47246a6fb8053901fdc7c3f2354c5fed4dc99
plaintexteditcounter <plaintexteditcounter.tom29739@tools.wmflabs.org>
2016-04-02 03:58:49 +0000

    Finally got tool working.

--- database.php
+++ database.php
@@ -1,19 +1,10 @@
 <?php
-$newl ='</br>';
-echo "Loaded PHP file.$newl";
-
-// Get dependencies
 require 'vendor/autoload.php';
 use Noodlehaus\Config;
-echo "Got dependencies.$newl";
-
-// Get database config file and populate db username and password variables from it
-$json = file_get_contents(https://en.wikipedia.org/w/api.php?action=query&format=json&list=users&usprop=editcount&ususers=tom29739);
-file_put_contents(json.json,$json);
+$username = htmlspecialchars($_GET["username"]);
+$json = file_get_contents("https://en.wikipedia.org/w/api.php?action=query&format=json&list=users&usprop=editcount&ususers=".$username);
+file_put_contents('json.json',$json);
 $conf = new Config('json.json');
-$editcount = $conf['query']['users']['editcount'];
-
-echo "Got config.$newl";
-echo $editcount.$newl;
-echo "Echoed data.$newl";
+$array = $conf['query']['users'];
+echo $array['0']['editcount'];
 ?>
