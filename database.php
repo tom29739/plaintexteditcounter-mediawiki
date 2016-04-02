6a8bb05932d5f950338612926458f3db25d9b7fd
tom29739 <tom29739@users.noreply.github.com>
2016-04-02 03:46:17 +0100

    Use library correctly.

--- database.php
+++ database.php
@@ -8,7 +8,9 @@ use Noodlehaus\Config;
 echo "Got dependencies.$newl";
 
 // Get database config file and populate db username and password variables from it
-$conf = new Config('https://en.wikipedia.org/w/api.php?action=query&format=json&list=users&usprop=editcount&ususers=tom29739');
+$json = file_get_contents(https://en.wikipedia.org/w/api.php?action=query&format=json&list=users&usprop=editcount&ususers=tom29739);
+file_put_contents(json.json,$json);
+$conf = new Config('json.json');
 $editcount = $conf['query']['users']['editcount'];
 
 echo "Got config.$newl";
